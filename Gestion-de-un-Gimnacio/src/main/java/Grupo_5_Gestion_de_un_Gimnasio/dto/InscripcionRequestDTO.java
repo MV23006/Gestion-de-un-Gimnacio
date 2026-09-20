ec5458ae4c4bc59e2a21172a55055289ef523c2f
MV23006 <mv23006@ues.edu.sv>
2026-09-20 00:02:49 -0600

    agregar README.md

--- Gestion-de-un-Gimnacio/src/main/java/Grupo_5_Gestion_de_un_Gimnasio/dto/InscripcionRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package Grupo_5_Gestion_de_un_Gimnasio.dto;
-
-import lombok.Data;
-
-@Data
-public class InscripcionRequestDTO {
-    private Long socioId;
-    private Long claseId;
-}
--- Gestion-de-un-Gimnacio/src/main/java/Grupo_5_Gestion_de_un_Gimnasio/repository/ClaseRepository.java
+++
@@ -1,9 +0,0 @@
-package Grupo_5_Gestion_de_un_Gimnasio.repository;
-
-import Grupo_5_Gestion_de_un_Gimnasio.model.Clase;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClaseRepository extends JpaRepository<Clase, Long> {
-}
