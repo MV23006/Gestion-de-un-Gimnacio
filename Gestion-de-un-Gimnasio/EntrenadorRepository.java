846a1fab0c5f8cfc1858f009270dd1116dd2a7d4
MV23006 <mv23006@ues.edu.sv>
2026-09-25 08:56:15 -0600

    feat: agregar modulo de pagos y ajustar paquetes

--- Gestion-de-un-Gimnasio/EntrenadorRepository.java
+++
@@ -1,9 +0,0 @@
-package Grupo_5_Gestion_de_un_Gimnasio.repository;
-
-import Grupo_5_Gestion_de_un_Gimnasio.model.Entrenador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntrenadorRepository extends JpaRepository<Entrenador, Long> {
-}
